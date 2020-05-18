5fc9b0c0f2f77f520ffb79862f36ecaae09b54c2
MartinIversen8 <martin-iversen@hotmail.com>
2020-05-18 13:09:25 +0200

    BOATS BOATS BOATS update

---
+++ OxbridgeRaceProject/ClientApp/src/app/map/map.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AgmCoreModule } from '@agm/core';
+
+@NgModule({
+  declarations: [
+   ]
+})
+

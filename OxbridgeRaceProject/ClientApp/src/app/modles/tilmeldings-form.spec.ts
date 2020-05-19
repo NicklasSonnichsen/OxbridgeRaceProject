8750976382fe7f21ff4aab786ad9cecab5b002a4
MartinIversen8 <martin-iversen@hotmail.com>
2020-05-19 10:35:45 +0200

    Everybody Chnages but the code seems the same

---
+++ OxbridgeRaceProject/ClientApp/src/app/modles/tilmeldings-form.spec.ts
@@ -0,0 +1,7 @@
+import { TilmeldingsForm } from './tilmeldings-form';
+
+describe('TilmeldingsForm', () => {
+  it('should create an instance', () => {
+    expect(new TilmeldingsForm()).toBeTruthy();
+  });
+});
